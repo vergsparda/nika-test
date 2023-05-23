29cde39f8b71094a08aaae795f2e80a748032965
Alexey Kuznetsov <alex.vesson.smith@gmail.com>
2023-05-23 15:37:55 +0300

    init project

---
+++ README.md
@@ -0,0 +1,16 @@
+# nika-test
+
+## Init project
+```
+npm install
+```
+
+### Run for development
+```
+npm run serve
+```
+
+### Run for release build
+```
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <PhotoApp msg="Тествое задание"/>
+</template>
+
+<script>
+import PhotoApp from './components/PhotoApp.vue';
+
+export default {
+  name: 'App',
+  components: {
+    PhotoApp
+    ,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
